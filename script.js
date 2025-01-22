1630b53d1c2f00d40957dc10e352389f98c2d961
Sucha25 <o.suchora@op.pl>
2025-01-22 20:03:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ document.addEventListener("DOMContentLoaded", () => {
     // Pokazanie smutnego kota
     responseDiv.innerHTML = `
       <img src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExM2o4ZndnMHN3NnlocDIzZTI1amRkOWdybXowbDIxb2Q1Y2J0YTRxbiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/wfS4vDyVsASQygl4mN/giphy.gif" alt="Smutny kot">
-      <p>I po co to klikasz? Nie ma takiej możliwości!</p>
+      <p>I po co to klikasz? Nie ma takiej możliwości!!</p>
     `;
     responseDiv.classList.remove("hidden");
   });
